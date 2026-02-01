d5b98c82b533d5c400abeec3deb7d5f14a893ce0
Huynh <Ngochuynhjp97@gmail.com>
2026-02-02 00:41:00 +0900

    hihi

---
+++ src/main/java/com/vti/testtingsystem1/Testtingsystem1Application.java
@@ -0,0 +1,13 @@
+package com.vti.testtingsystem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testtingsystem1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testtingsystem1Application.class, args);
+    }
+
+}
